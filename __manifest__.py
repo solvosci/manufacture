785a0c038ac9c9cd22a52197c67ca6dad1ab859c
David <david.alonso@solvos.es>
2018-06-04 23:01:42 +0200

    Scale: created basic model for weighing scales

--- __manifest__.py
+++ __manifest__.py
@@ -24,7 +24,8 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
+        'views/scale_views.xml',
+        'views/menuitem.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
---
+++ models/scale.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+
+import socket
+
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
+
+# TODO make it CONST
+SCALE_RECV_BUFFER_SIZE = 1024
+
+
+class Scale(models.Model):
+    '''
+    Represents a Weighing Scale managed over TCP/IP
+    '''
+    _name = 'slv.mdc.scale'
+    _description = 'Weighing Scale'
+
+    name = fields.Char(
+        'Name',
+        required=True)
+    tcp_address_ip = fields.Char(
+        'IP Address',
+        required=True)
+    tcp_address_port = fields.Integer(
+        'IP Port',
+        required=True)
+    scale_protocol = fields.Selection(
+        [('$', '$ Protocol')],
+        string='Weighing protocol',
+        required=True,
+        default='$')
+    # TODO uom field (g, kg,...)
+    last_weight_value = fields.Float(
+        'Last weight value',
+        readonly=True,
+        copy=False)
+    last_weight_stability = fields.Selection(
+        [('stable', 'Stable'), ('unstable', 'Unstable'), ('unknown', 'Unknown')],
+        string='Last weight stability',
+        readonly=True,
+        copy=False)
+    last_weight_datetime = fields.Datetime(
+        'Last weight date',
+        readonly=True,
+        copy=False)
+    # TODO last weight uom, waiting timeout
+    active = fields.Boolean(
+        'Active',
+        default=True)
+    # TODO current line
+
+    # TODO ensure data validation (IP address,...) when creating and writing a record
+
+    @api.multi
+    def action_get_weight(self):
+        '''
+        Triggered by a form button, gets the current weight
+        :return:
+        '''
+        self.ensure_one()
+        try:
+            self.get_weight()
+            return True
+        except Exception as err:
+            raise UserError(err)
+
+    def get_weight(self):
+        '''
+        Gets the current weight
+        :return:
+        '''
+        # TODO waiting timeout
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((self.tcp_address_ip, self.tcp_address_port))
+            if self.scale_protocol == '$':
+                self.last_weight_value, self.last_weight_stability, self.last_weight_datetime \
+                    = self._get_weight_s_protocol(s)
+                return self.last_weight_value, self.last_weight_stability, self.last_weight_datetime
+            else:
+                raise Exception(_('Unsupported weighing protocol (%s)') % self.protocol)
+
+    def _get_weight_s_protocol(self, sock):
+        sock.send(b'$')
+        data = sock.recv(SCALE_RECV_BUFFER_SIZE)
+        # STX = 0x02 ; other control data is not set
+        if len(data) == 10 and data[0] == 2:
+            weight = float(data[2:])
+            stability = 'stable' if '{0:b}'.format(data[1])[-6] == '0' else 'unstable'
+            return weight, stability, fields.Datetime.now()
+        else:
+            raise UserError(_('Unknown data format. Data received: %s') % data)
