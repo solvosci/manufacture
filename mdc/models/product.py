fbdeab7500f3c975015159b227850799943d9d02
Luisa <luisa.miguens@solvos.es>
2018-07-13 09:35:21 +0200

    Product: rewrite name_get to display always attribute name, when there is only one attribute too

--- mdc/models/product.py
+++ mdc/models/product.py
@@ -19,7 +19,71 @@ class ClassName(models.TransientModel):
 
     @api.multi
     def set_key_values(self):
+       #TODO: it does not work
+       #we want to put the parameter group_product_variant with value True (to have variant in products)
        self.ensure_one()
        ICP = self.env['ir.config_parameter']
        ICP.set_param('key', self.key)
        ICP.set_param('value', self.value)
+
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    @api.multi
+    def name_get(self):
+        # TDE: this could be cleaned a bit I think
+
+        def _name_get(d):
+            name = d.get('name', '')
+            code = self._context.get('display_default_code', True) and d.get('default_code', False) or False
+            if code:
+                name = '[%s] %s' % (code, name)
+            return (d['id'], name)
+
+        partner_id = self._context.get('partner_id')
+        if partner_id:
+            partner_ids = [partner_id, self.env['res.partner'].browse(partner_id).commercial_partner_id.id]
+        else:
+            partner_ids = []
+
+        # all user don't have access to seller and partner
+        # check access and use superuser
+        self.check_access_rights("read")
+        self.check_access_rule("read")
+
+        result = []
+        for product in self.sudo():
+            # display only the attributes with multiple possible values on the template
+            # variable_attributes = product.attribute_line_ids.filtered(lambda l: len(l.value_ids) > 1).mapped('attribute_id')
+            # we need display always attribute name, when there is only one attribute too
+            variable_attributes = product.attribute_line_ids.mapped('attribute_id')
+            variant = product.attribute_value_ids._variant_name(variable_attributes)
+
+            name = variant and "%s (%s)" % (product.name, variant) or product.name
+            sellers = []
+            if partner_ids:
+                sellers = [x for x in product.seller_ids if (x.name.id in partner_ids) and (x.product_id == product)]
+                if not sellers:
+                    sellers = [x for x in product.seller_ids if (x.name.id in partner_ids) and not x.product_id]
+            if sellers:
+                for s in sellers:
+                    seller_variant = s.product_name and (
+                            variant and "%s (%s)" % (s.product_name, variant) or s.product_name
+                    ) or False
+                    mydict = {
+                        'id': product.id,
+                        'name': seller_variant or name,
+                        'default_code': s.product_code or product.default_code,
+                    }
+                    temp = _name_get(mydict)
+                    if temp not in result:
+                        result.append(temp)
+            else:
+                mydict = {
+                    'id': product.id,
+                    'name': name,
+                    'default_code': product.default_code,
+                }
+                result.append(_name_get(mydict))
+        return result
