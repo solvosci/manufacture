b08f56088ed5d0b2a544d8b645130ff7fb836a88
David Alonso <david.alonso@solvos.es>
2018-10-10 16:25:08 +0200

    [FIX] Product: enabled by default variant management // Demo product variant creation

--- mdc/models/product.py
+++ mdc/models/product.py
@@ -4,29 +4,6 @@ from odoo import api, fields, models
 _logger = logging.getLogger(__name__)
 
 
-class ClassName(models.TransientModel):
-    _inherit = 'res.config.settings'
-
-    key = fields.Char()
-    value = fields.Char()
-
-    @api.model
-    def get_default_key_values(self, fields):
-        return {
-            'key': "group_product_variant",
-            'value': "True",
-        }
-
-    @api.multi
-    def set_key_values(self):
-       #TODO: it does not work
-       #we want to put the parameter group_product_variant with value True (to have variant in products)
-       self.ensure_one()
-       ICP = self.env['ir.config_parameter']
-       ICP.set_param('key', self.key)
-       ICP.set_param('value', self.value)
-
-
 class ProductProduct(models.Model):
     _inherit = 'product.product'
 
