385d7fbf07dc690f6b0829ffad15804e592a0f15
resua <32322359+adriresu@users.noreply.github.com>
2022-09-19 13:42:14 +0200

    [FIX] mdc_crumbs: Set bootstrap css local

--- mdc_crumbs/__manifest__.py
+++ mdc_crumbs/__manifest__.py
@@ -8,7 +8,7 @@
     """,
     "author": "Solvos",
     "license": "LGPL-3",
-    "version": "11.0.1.0.0",
+    "version": "11.0.1.0.1",
     "category": "Manufacturing",
     "website": "https://github.com/solvosci/manufacture",
     "depends": [
